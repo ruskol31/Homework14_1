7b9ee4679c7e602f10f7373376ec1987bac1fce1
Ruskol31 <rk31@yandex.ru>
2025-12-15 17:09:42 +0300

    start commit

---
+++ src/Category.py
@@ -0,0 +1,16 @@
+class Category:
+    name: str
+    description: str
+    products: str
+    category_count = 0
+    product_count = 0
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
+        Category.category_count +=1
+        Product_count +=
+        
+
+
+
